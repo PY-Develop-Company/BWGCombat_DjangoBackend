44431fe194ebeb920ab3a378a9e41b8d4d810b9f
Vitalii Lutsyk <vitaliilutsyk9895@gmail.com>
2024-06-19 16:13:35 +0300

    added user_data_table. Coins tokens and also pre-tables for tasks, stages and ranks

---
+++ app/userdata/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import UserData, Rank, Reward, Stage, Task
+
+# Register your models here.
+
+class UserDataAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'gold_balance', 'g_token', 'rank_id', 'stage_id')
+    list_select_related = ('rank_id',)
+
+
+class RankAdmin(admin.ModelAdmin):
+    list_display = ('name', 'reward_id')
+    list_select_related = ('reward_id',)
+
+class RewardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount')
+
+
+
+admin.site.register(UserData, UserDataAdmin)
+admin.site.register(Rank, RankAdmin)
+admin.site.register(Reward, RewardAdmin)
+admin.site.register(Stage)
+admin.site.register(Task)
