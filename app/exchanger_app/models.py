6d6218765371f3458e3ea0bdfbedf453adcddea4
Dimon Stelmakh <105310426+DimonStelmakh@users.noreply.github.com>
2024-07-31 00:55:31 +0300

    Swap model updated (user_rank added)

--- app/exchanger_app/models.py
+++ app/exchanger_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from user_app.models import User
+from levels_app.models import Rank
 from django.utils import timezone
 
 
@@ -9,6 +10,7 @@ class Asset(models.Model):
 
 class Swap(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user_rank = models.ForeignKey(Rank, on_delete=models.DO_NOTHING)
     asset_1 = models.ForeignKey(Asset, on_delete=models.DO_NOTHING, related_name='swaps_giving')
     asset_2 = models.ForeignKey(Asset, on_delete=models.DO_NOTHING, related_name='swaps_receiving')
     fee = models.FloatField(default=0.0)
